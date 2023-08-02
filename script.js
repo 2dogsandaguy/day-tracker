a6012ddc0d5cc97fb2959a1bd12f875a1fecb6fa
2dogsandaguy <Neverclear@ymail.com>
2023-08-02 18:23:14 -0400

    cleaned up js & html and write comments in js to get better grasp on things

--- script.js
+++ script.js
@@ -1,124 +1,40 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-/*$(document).ready(function () {
-    var NowMoment = moment().format("MMMM Do YYYY");
-  var displayDate = document.querySelector("currentDay");
-  displayDate.innerHTML = NowMoment;
-  var currentHour = moment().format("HH");
-  console.log(timeBlock)
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-  
-    $(".time-div").each(function () {
-      var timeDiv = $(this).attr("id").split("-")[1];
-      
-      if (currentHour == timeDiv) {
-        $(this).addClass("present");
-        $(this).children(".description").addClass("white-text");
-      } else if (currentHour < timeDiv) {
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      } else if (currentHour > timeDiv) {
-        $(this).removeClass("future");
-        $(this).addClass("past");
-      }
-    });
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  }); */
-
-  
- /* document.addEventListener("DOMContentLoaded", function () {
-    // Moment.js code for current date and time
-    var nowMoment = moment().format("MMMM Do YYYY");
-    var displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = nowMoment;
-  
-    var currentHour = moment().format("HH");
-  
-    // Button function to clear local storage and clear contents
-    /*var clearFieldsBtn = document.getElementById("clearFieldsBtn");
-    clearFieldsBtn.addEventListener("click", function (event) {
-      event.preventDefault();
-      var textareas = document.querySelectorAll("textarea");
-      textareas.forEach(function (textarea) {
-        textarea.value = "";
-      });
-      localStorage.clear();
-    });
-  
-    // Grabs hour from each time slot and compares it to the actual time
-    var timeDivs = document.querySelectorAll(".time-block");
-    timeDivs.forEach(function (timeBlock) {
-      var timeDivId = parseInt(timeBlock.getAttribute("id").split("-")[1]);
-      var description = timeBlock.querySelector(".description");
-  
-      if (currentHour == timeId) {
-        timeBlock.classList.add("present");
-        /*description.classList.add("white-text");
-      } else if (currentHour < timeDivId) {
-        timeBlock.classList.remove("present");
-        timeBlock.classList.add("future");
-      } else {
-        timeBlock.classList.remove("future");
-        timeBlock.classList.add("past");
-      }
-    });*/
-
- 
-// Define a function 'logColors'
+// Define a function 'logColors' to call the function later
 var logColors = function () {
-    // Get the current hour using 'dayjs' library withc is getting the hours of the day
+    // Get the current hour using 'dayjs' library with is getting the hour of the day 'hour'
     var nowHour = dayjs().hour();
     
     // Get all elements with IDs starting with 'hour-' and store them in 'plannerHours' the (^)is start with
     var plannerHours = $('[id^="hour-"]');
     
-    // Iterate over each element in 'plannerHours'
+    // repeat over each element in 'plannerHours'
     plannerHours.each(function () {
       // Get the ID attribute of the current element
-      const id = $(this).attr("id");
+      var id = $(this).attr("id");
       
       // Extract the number at the end of the ID by splitting at the '-' character
-      const endingNumber = id.split("-")[1];
+      var endingNumber = id.split("-")[1];
   
       // Compare the extracted number with 'nowHour' and apply classes based on the comparison
       if (parseInt(endingNumber) > nowHour) {
-        $(this).addClass("future");   // Add the 'future' class to the current element
+        $(this).addClass("future");   // Add the 'future' class to the current element green
       } else if (parseInt(endingNumber) === nowHour) {
-        $(this).addClass("present");  // Add the 'present' class to the current element
+        $(this).addClass("present");  // Add the 'present' class to the current element red
       } else if (parseInt(endingNumber) < nowHour) {
-        $(this).addClass("past");     // Add the 'past' class to the current element
+        $(this).addClass("past");     // Add the 'past' class to the current element gray
       }
     });
   };
   
-  // Call the 'logColors' function
+  // Call the 'logColors' function line 2
   logColors();
   
-  // Output the 'logColors' function to the console
-  console.log(logColors())
   
   // Define a function to save tasks to localStorage
 function saveTasksToLocalStorage() {
-    // Retrieve existing tasks from localStorage or initialize an empty array if it doesn't exist
+    // Retrieve existing tasks from localStorage or initialize an empty array if it doesn't exist 
     var existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
   
-    // Iterate over each element with class 'time-block'
+    // repeat over each element with class 'time-block'
     $(".time-block").each(function () {
       // Get the ID of the current element and store it in 'timerText'
       var timerText = $(this).attr("id");
@@ -192,7 +108,7 @@ function saveTasksToLocalStorage() {
 
 
 
-    //this works but does not store localstorage in array
+    //this works but does not store localstorage in array was messy in localstorage
   
 /*// Add a click event listener to all elements with the class 'saveBtn'
   $(".saveBtn").on("click", function () {
@@ -220,39 +136,25 @@ function saveTasksToLocalStorage() {
   }); */
 
 
-/*function that i want to add array for the localstorage does not save 11am
+/*function that i want to add array for the localstorage does not save 11am 
   /*var tasks = [];
 
   $(".saveBtn").on("click", function () {
-    
     var textSlots = $(this).parent().attr("id");
-  
-  
     var fillerTask = $(this).siblings(".description").val().trim();
-  
-  
     var existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  
-   
     existingTasks.push({ id: textSlots, task: fillerTask });
-  
     localStorage.setItem("tasks", JSON.stringify(existingTasks));
   });
-  
   
   $(function () {
    
     var existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  
     $(".time-block").each(function () {
-      
       var timerText = $(this).attr("id");
-  
-      
       var task = existingTasks.find(function (item) {
         return item.id === timerText;
       });
-  
   
       if (task) {
         $(this).children(".description").val(task.task);
